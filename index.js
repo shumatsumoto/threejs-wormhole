9484eba7511eb9fe76d6f50dc1c490c3019fdf90
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-07-25 13:27:54 +0900

    points

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.set(0.5, 0.5, 15);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-scene.fog = new THREE.FogExp2(0x000000, 0.025);
+scene.fog = new THREE.FogExp2(0x000000, 0.01);
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
@@ -22,15 +22,16 @@ const geo = new THREE.CylinderGeometry(
   256,
   true
 );
-const mat = new THREE.MeshNormalMaterial();
-const tube = new THREE.Mesh(geo, mat);
-scene.add(tube);
+const mat = new THREE.PointsMaterial({ color: 0x0099ff, size: 0.1 });
+const points = new THREE.Points(geo, mat);
+points.rotation.x = Math.PI / 2;
+scene.add(points);
 
 function animate(t) {
   requestAnimationFrame(animate);
   // camera.position.x = Math.cos(t * 0.001) * 1.5;
   // camera.position.y = Math.sin(t * 0.001) * 1.5;
-  tube.rotation.y = t * 0.0005;
+  points.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 
